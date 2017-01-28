260bb774938b9f9bf214df30d0e5d17b3b7ef606
sundersb <m_sun@mail.ru>
2017-01-28 22:24:42 +1000

    Added DateTime class files

---
+++ date.cpp
@@ -0,0 +1,500 @@
+#include "date.h"
+#include <sstream>
+#include <iomanip>
+#include <climits>
+
+using namespace std;
+
+// Year for which tm::tm_year is zero:
+const int TM_START_YEAR = 1900;
+
+// Seconds to milliseconds quotient
+const int TIME_MULTIPLIER = 1000;
+
+const int MONTH_COUNT = 12;
+const int SECS_IN_MINUTE = 60;
+const int SECS_IN_HOUR = SECS_IN_MINUTE * 60;
+const int SECS_IN_DAY = SECS_IN_HOUR * 24;
+const long long MILLISECS_IN_DAY = SECS_IN_DAY * TIME_MULTIPLIER;
+
+// Displacement of time_t ticks related to DateTime::m_time
+const long long TIME_T_ZERO = 62167132800000;
+
+const int MONTH_LENGTHS[]      =     {31, 28, 31, 30,   31,  30,  31,  31,  30,  31,  30,  31};
+const int MONTH_LENGTHS_LEAP[] =     {31, 29, 31, 30,   31,  30,  31,  31,  30,  31,  30,  31};
+
+const int MONTH_STARTS[]       = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+
+// Spare day of the leap year counted in the leap-year-days
+// So it's compensated after February but lacks in Jan & Feb
+const int MONTH_STARTS_LEAP[]  = {-1, 30, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+
+/** Get difference between local time and UTC time in milliseconds
+ */
+long long getTimezone(void) {
+  static bool inited(false);
+  static long long tz(0);
+
+  if (!inited) {
+    // Point is converting current time_t to UTC in struct tm, then back to localtime
+    time_t t = time(nullptr);
+    tm utc;
+    if (gmtime_s(&utc, &t) == 0) {
+      // Unknown state of daylight saving (supposing the state is not
+      // changing while the application works):
+      utc.tm_isdst = -1;
+
+      time_t tu = mktime(&utc);
+      tz = (tu - t) * TIME_MULTIPLIER;
+    }
+    inited = true;
+  }
+
+  return tz;
+}
+
+/** Check if the year is leap
+ */
+inline bool isLeap (int year) {
+  // Gregorian
+  return (year % 4 == 0)
+    && ((year % 100 != 0) || (year % 400 == 0));
+}
+
+/** Count leap days in the years
+ */
+inline int getLeapDays(int years) {
+  // Gregorian
+  return years / 4 - years / 100 + years / 400;
+}
+
+/** Quick and dirty replacement for struct tm with
+ *  additional functionality
+ */
+class STime {
+  int year;
+  int month;  // 0 - 11
+  int day;    // 1 - xx
+  int hour;
+  int minute;
+  int second;
+  int millisecond;
+  bool valid;
+
+  // Extract month and day from given monthTable (MONTH_STARTS or MONTH_STARTS_LEAP)
+  void setYearsDay(const int *monthTable, const int *lengths, int yearDay);
+
+public:
+  STime();
+  STime(long long time);
+  STime(const tm *time);
+  STime(const string &value);
+  
+  // Get amount of milliseconds from J1n, 1 of the 1'th year
+  long long get(void);
+
+  // Returns day-of-year for the date
+  int dayOfYear(void) const;
+
+  // Add months to the date-time
+  void incMonth(int months) { month += months; }
+
+  // Add years to the date-time
+  void incYear(int years) { year += years; }
+
+  // Get date formatted as yyyy-MM-dd
+  string formatDate(void) const;
+
+  // Get date and time formatted as yyyy-MM-dd hh:mm:ss
+  string formatDateTime(void) const;
+
+  // Get amount of months since a previous date
+  // (this date-time MUST be before or equal to the "from" one)
+  int monthsAfter(const STime &from);
+};
+
+STime::STime():
+  year(0),
+  month(0),
+  day(0),
+  hour(0),
+  minute(0),
+  second(0),
+  millisecond(0),
+  valid(true)
+{}
+
+STime::STime(const tm *time)
+{
+  if (time) {
+    year = time->tm_year + TM_START_YEAR;
+    month = time->tm_mon;
+    day = time->tm_mday;
+    hour = time->tm_hour;
+    minute = time->tm_min;
+    second = time->tm_sec;
+    millisecond = 0;
+
+    valid = year >= TM_START_YEAR
+      && day > 0
+      && day <= MONTH_LENGTHS[month]
+      && hour >= 0
+      && minute >= 0
+      && second >= 0;
+  } else {
+    memset(this, 0, sizeof(STime));
+    valid = false;
+  }
+}
+
+STime::STime(const string &value) {
+  // Read the fields from a string
+  memset (this, 0, sizeof(STime));
+  char dummy;
+  istringstream str(value);
+
+  str >> year;
+  valid = !str.fail();
+
+  str >> dummy >> month;
+  valid &= !str.fail();
+
+  str >> dummy >> day;
+  valid &= !str.fail();
+
+  if (!str.eof()) {
+    // The string includes time portion
+    if (!(str >> hour)
+      || !(str >> dummy >> minute)
+      || !(str >> dummy >> second)) 
+        valid = false;
+
+    str >> dummy >> millisecond;
+  }
+
+  // Month starts from zero:
+  month--;
+}
+
+STime::STime (long long time): valid(true) {
+  millisecond = time % TIME_MULTIPLIER;
+  // Milliseconds ingnored
+  time /= TIME_MULTIPLIER;
+
+  // Seconds since the day start
+  second = time % SECS_IN_DAY;
+
+  hour = second / SECS_IN_HOUR;
+  second %= SECS_IN_HOUR; 
+
+  minute = second / SECS_IN_MINUTE;
+  second %= SECS_IN_MINUTE;
+  // ... now everything within the day is correct
+
+  // Amount of days since 1.01.01
+  time /= SECS_IN_DAY;
+
+  // Maximum possible years count for this amount of days:
+  year = time / 365;
+  year -= getLeapDays(year) / 365;
+
+  // The year changed, leap days count may be changed also:
+  int leap = getLeapDays(year);
+
+  // Day of the year for these year and leap days count:
+  long long days = time - (long long)year * 365 - leap;
+  
+  // Adjusting the year to achieve days being in [0; 365]
+  // Down:
+  while (days < 0) {
+    year--;
+    leap = getLeapDays(year);
+    days = time - (long long)year * 365 - leap;
+  }
+
+  // Up:
+  while (days > 365) {
+    year++;
+    leap = getLeapDays(year);
+    days = time - (long long)year * 365 - leap;
+  }
+
+  // Split day-of-the year onto month and day
+  if (isLeap(year))
+    setYearsDay(MONTH_STARTS_LEAP, MONTH_LENGTHS_LEAP, days);
+  else
+    setYearsDay(MONTH_STARTS, MONTH_LENGTHS, days);
+}
+
+void STime::setYearsDay(const int *monthTable, const int *lengths, int yearDay) {
+  for (int i = 1; i < 13; i++) {
+    if (yearDay <= monthTable[i]) {
+      month = i-1;
+      day = yearDay - monthTable[i-1] + 1;
+      break;
+    }
+  }
+
+  // Lame date correction for the cases like Jan, 32
+  if (day > lengths[month]) {
+    day = 1;
+    month++;
+    if (month > 11) {
+      year++;
+      month = 0;
+    }
+  }
+}
+
+long long STime::get(void) {
+  if (!valid) return LLONG_MIN;
+
+  // By chance, month may be more than 11...
+  year += month / MONTH_COUNT;
+  month %= MONTH_COUNT;
+
+  int mdays;
+  if (isLeap(year))
+    mdays = MONTH_STARTS_LEAP[month];
+  else
+    mdays = MONTH_STARTS[month];
+
+  // Days amount from Jan, 1 of the 1'st year
+  long long result = year * 365 // in non-leap years
+    + getLeapDays(year)         // days in leap years
+    + mdays                     // the day on which the month starts
+    + day                       // month's day
+    - 1;                        // since month starts with day 1 (not 0)
+
+  result *= SECS_IN_DAY;
+  result += hour * SECS_IN_HOUR;
+  result += minute * SECS_IN_MINUTE;
+  result += second;
+  result *= TIME_MULTIPLIER;
+  result += millisecond;
+
+  return result;
+}
+
+string STime::formatDate(void) const {
+  ostringstream res;
+  res << setfill('0') << year
+    << '-' << setw(2) << (month + 1)
+    << '-' << setw(2) << day;
+  return res.str();
+}
+
+string STime::formatDateTime(void) const {
+  ostringstream res;
+  res << setfill('0') << year
+    << '-' << setw(2) << (month + 1)
+    << '-' << setw(2) << day
+    << ' ' << setw(2) << hour
+    << ':' << setw(2) << minute
+    << ':' << setw(2) << second;
+
+  if (millisecond) res << '.' << setw(3) << millisecond;
+
+  return res.str();
+}
+
+int STime::dayOfYear(void) const {
+  if (isLeap(year))
+    return MONTH_STARTS_LEAP[month] + day;
+  else
+    return MONTH_STARTS[month] + day;
+}
+
+int STime::monthsAfter(const STime &from) {
+  // * * * > *
+  // * * * * *
+  // * < * * *
+  int result = (year - from.year) * MONTH_COUNT;
+  result -= from.month - month;
+
+  if (day < from.day)
+    return result - 1;
+
+  if (day == from.day) {
+    int time = (hour * SECS_IN_HOUR + minute * SECS_IN_MINUTE + second) * TIME_MULTIPLIER + millisecond;
+    int ftime = (from.hour * SECS_IN_HOUR + from.minute * SECS_IN_MINUTE + from.second) * TIME_MULTIPLIER + from.millisecond;
+    if (ftime > time)
+      return result - 1;
+  }
+
+  return result;
+}
+
+//******************************
+DateTime::DateTime () {
+  // Invalid date-time by default
+  m_time = LLONG_MIN;
+}
+
+DateTime::DateTime (const DateTime &value):
+  m_time(value.m_time)
+{}
+
+DateTime::DateTime (const std::string &value) {
+  set(value);
+}
+
+DateTime::DateTime (const time_t &time) {
+  set(time);
+}
+
+DateTime::DateTime (const tm *time) {
+  set(time);
+}
+
+void DateTime::setNow(void) {
+  set(time(nullptr));
+}
+
+bool DateTime::isValid(void) const {
+  return m_time != LLONG_MIN;
+}
+
+long long DateTime::getRaw(void) const {
+  return m_time;
+}
+
+void DateTime::toUTC(void) {
+  if (m_time != LLONG_MIN)
+    m_time += getTimezone();
+}
+
+void DateTime::fromUTC(void) {
+  if (m_time != LLONG_MIN)
+    m_time -= getTimezone();
+}
+
+std::string DateTime::formatDate(void) const {
+  if (m_time == LLONG_MIN) return string();
+  STime time(m_time);
+  return time.formatDate();
+}
+
+std::string DateTime::formatDateTime(void) const {
+  if (m_time == LLONG_MIN) return string();
+  STime time(m_time);
+  return time.formatDateTime();
+}
+
+time_t DateTime::asUnixTime(void) const {
+  if (m_time == LLONG_MIN) return -1;
+  time_t result = (m_time - TIME_T_ZERO) / TIME_MULTIPLIER;
+  if (result < 0) return -1;
+  return result;
+}
+
+void DateTime::set (const std::string &value) {
+  STime time(value);
+  m_time = time.get();
+}
+
+void DateTime::set (const time_t &time) {
+  if (time > 0) {
+    m_time = (long long) time * TIME_MULTIPLIER + TIME_T_ZERO;
+  } else {
+    m_time = LLONG_MIN;
+  }
+}
+
+void DateTime::set (const tm *time) {
+  STime t(time);
+  m_time = t.get();
+}
+
+DateTime& DateTime::incSecond(int seconds) {
+  if (m_time != LLONG_MIN)
+    m_time += (long long) seconds * TIME_MULTIPLIER;
+  return *this;
+}
+
+DateTime& DateTime::incMinute(int minutes) {
+  if (m_time != LLONG_MIN)
+    m_time += (long long) minutes * SECS_IN_MINUTE * TIME_MULTIPLIER;
+  return *this;
+}
+
+DateTime& DateTime::incHour(int hours) {
+  if (m_time != LLONG_MIN)
+    m_time += (long long) hours * SECS_IN_HOUR * TIME_MULTIPLIER;
+  return *this;
+}
+
+DateTime& DateTime::incDay(int days) {
+  if (m_time != LLONG_MIN)
+    m_time += (long long) days * MILLISECS_IN_DAY;
+  return *this;
+}
+
+DateTime& DateTime::incMonth(int months) {
+  if (m_time != LLONG_MIN) {
+    STime time(m_time);
+    time.incMonth(months);
+    m_time = time.get();
+  }
+  return *this;
+}
+
+DateTime& DateTime::incYear(int years) {
+  if (m_time != LLONG_MIN) {
+    STime time(m_time);
+    time.incYear(years);
+    m_time = time.get();
+  }
+  return *this;
+}
+
+int DateTime::getWeekDay(void) const {
+  if (m_time != LLONG_MIN)
+    return (m_time / MILLISECS_IN_DAY + 6) % 7;
+  else
+    return -1;
+}
+
+int DateTime::getDayOfYear(void) const {
+  if (m_time == LLONG_MIN) return -1;
+  STime time(m_time);
+  return time.dayOfYear();
+}
+
+int DateTime::daysBetween(const DateTime &date1, const DateTime &date2) {
+  if (date1.m_time == LLONG_MIN || date2.m_time == LLONG_MIN) return -1;
+  long long diff = date1.m_time - date2.m_time;
+  if (diff < 0) diff = -diff;
+  return diff / MILLISECS_IN_DAY;
+}
+
+int DateTime::monthsBetween(const DateTime &date1, const DateTime &date2) {
+  if (date1.m_time == LLONG_MIN || date2.m_time == LLONG_MIN) return -1;
+  if (date1 == date2) return 0;
+
+  STime d1(date1.m_time);
+  STime d2(date2.m_time);
+
+  if (date1 < date2)
+    return d2.monthsAfter(d1);
+  else
+    return d1.monthsAfter(d2);
+}
+
+int DateTime::yearsBetween(const DateTime &date1, const DateTime &date2) {
+  int months = monthsBetween(date1, date2);
+  if (months <= 0)
+    return months;
+  else
+    return months / MONTH_COUNT;
+}
+
+const DateTime& DateTime::operator = (const DateTime &date) {
+  m_time = date.m_time;
+  return *this;
+}
+
+bool DateTime::hasTime(void) const {
+	return m_time != LLONG_MIN
+		&& (m_time % MILLISECS_IN_DAY) != 0;
+}
---
+++ date.h
@@ -0,0 +1,202 @@
+#pragma once
+#include <ctime>
+#include <string>
+
+/** Date and time class
+ *  Keeps date and time value from start of year 1 AC by Gregorian counting
+ *  Checked to work till year 10000
+ */
+class DateTime {
+  /** Millisecnods since the start of January, 1 of the 1'st year
+   */
+  long long m_time;
+
+public:
+  /** Default constructor. Sets the instance to invalid date and time
+   */
+  DateTime ();
+
+  /** Copy constructor
+   */
+  DateTime (const DateTime &value);
+
+  /** Construct DateTime value from SQL-formatted UTC date and time
+   * /param value       SQL-formatted date and time: "2017-01-17 17:19:21.012"
+   */
+  DateTime (const std::string &value);
+
+  /** Construct DateTime instance from a time_t value
+   * /param time        Seconds from UNIX-epoch start in UTC
+   */
+  DateTime (const time_t &time);
+
+  /** Construct DateTime value from struct *tm
+   * /param time        struct std::tm date-time object
+   */
+  DateTime (const tm *time);
+
+  /** Check validity of the date-time value of this instance
+   * /result      False if the instance does not contain valid date and time
+   */
+  bool isValid(void) const;
+
+  /** Return raw date and time value of this instance
+   * /result      Milliseconds from Jan, 1 of the 1'st year
+   */
+  long long getRaw(void) const;
+
+  /** Convert local datetime value to UTC
+   *  DateTime doesn't keep timezone for current value,
+   *   caller should track it by itself
+   */
+  void toUTC(void);
+
+  /** Convert UTC datetime to local
+   *  DateTime doesn't keep timezone for current value,
+   *   caller should track it by itself
+   */
+  void fromUTC(void);
+
+  /** Get date-time
+   * /returns       Date and time in UNIX time_t format
+   */
+  time_t asUnixTime(void) const;
+
+  /** Set value to current date and time (UTC)
+   */
+  void setNow(void);
+
+	/** Check does this date-time value contain time portion
+	 * /returns				True if this value has time portion
+	 */
+	bool hasTime(void) const;
+
+
+  /** Set date and time
+   * /param value      SQL-formatted UTC date and time: "2017-01-17 17:19:21"
+   */
+  void set (const std::string &value);
+
+  /** Set date and time
+   * /param time      A time_t (milliseconds from UNIX epoch) value
+   */
+  void set (const time_t &time);
+
+  /** Set date and time
+   * /param time      Date and time in a tm format
+   */
+  void set (const tm *time);
+
+
+  /* Get formatted date
+   * /result          Date in SQL-format (yyyy-MM-dd) or empty string for invalid dates
+   */
+  std::string formatDate(void) const;
+
+  /** Get formatted date and time
+   * /result          Date and time in SQL format (yyyy-MM-dd hh:mm:ss) or empty string for invalid dates
+   */
+  std::string formatDateTime(void) const;
+
+
+  /** Increase date and time by certain amount of seconds
+   *  /param seconds     Amount of seconds by which to increment/decrement current date-time
+   *   Negative parameter means subtruction
+   */
+  DateTime& incSecond(int seconds);
+
+  /** Increase date and time by certain amount of minutes
+   *  /param minutes     Amount of minutes by which to increment/decrement current date-time
+   *   Negative parameter means subtruction
+   */
+  DateTime& incMinute(int minutes);
+
+  /** Increase date and time by certain amount of hours
+   *  /param hours     Amount of hours by which to increment/decrement current date-time
+   *   Negative parameter means subtruction
+   */
+  DateTime& incHour(int hours);
+
+  /** Increase date and time by certain amount of days
+   *  /param days     Amount of days by which to increment/decrement current date-time
+   *   Negative parameter means days' subtruction
+   */
+  DateTime& incDay(int days);
+
+  /** Increase date and time by given amount of months
+   * /param months      Amount of months by which current daate should be incremented/decremented
+   *   May be negative (for months' subtraction) and can exceed a single year diapasone.
+   */
+  DateTime& incMonth(int months);
+
+  /** Increase date and time by given amount of years
+   * /param years      Amount of years by which current daate should be incremented/decremented
+   *   May be negative (for subtraction).
+   */
+  DateTime& incYear(int years);
+
+  /** Get weekday of the date
+   * /result      Weekday of a valid date (0 for Mon, 6 for Sun) or -1
+   */
+  int getWeekDay(void) const;
+
+  /** Get day of the year
+   * /result      Day of the year of a valid date or -1 for the invalid
+   */
+  int getDayOfYear(void) const;
+
+  /** Get amount of days between two DateTime values
+   * /param date1       First date
+   * /param date2       Second date
+   *  Returns amount of whole days between two valid dates or -1 otherwise
+   */
+  static int daysBetween(const DateTime &date1, const DateTime &date2);
+
+  /** Get amount of months between two DateTime values
+   * /param date1       First date
+   * /param date2       Second date
+   *  Returns amount of whole months between two valid dates or -1 otherwise
+   */
+  static int monthsBetween(const DateTime &date1, const DateTime &date2);
+
+  /** Get amount of years between two DateTime values
+   * /param date1       First date
+   * /param date2       Second date
+   *  Returns amount of whole years between two valid dates or -1 otherwise
+   */
+  static int yearsBetween(const DateTime &date1, const DateTime &date2);
+
+  const DateTime& operator = (const DateTime &date);
+
+  friend bool operator == (const DateTime &date1, const DateTime &date2);
+  friend bool operator < (const DateTime &date1, const DateTime &date2);
+  friend bool operator != (const DateTime &date1, const DateTime &date2);
+  friend bool operator <= (const DateTime &date1, const DateTime &date2);
+  friend bool operator > (const DateTime &date1, const DateTime &date2);
+  friend bool operator >= (const DateTime &date1, const DateTime &date2);
+};
+
+
+inline bool operator == (const DateTime &date1, const DateTime &date2) {
+  return date1.m_time == date2.m_time;
+}
+
+inline bool operator < (const DateTime &date1, const DateTime &date2) {
+  return date1.m_time < date2.m_time;
+}
+
+inline bool operator != (const DateTime &date1, const DateTime &date2) {
+  return !(date1 == date2);
+}
+
+inline bool operator <= (const DateTime &date1, const DateTime &date2) {
+  return date1 < date2 || date1 == date2;
+}
+
+inline bool operator > (const DateTime &date1, const DateTime &date2) {
+  return !(date1 <= date2);
+}
+
+inline bool operator >= (const DateTime &date1, const DateTime &date2) {
+  return !(date1 < date2);
+}
